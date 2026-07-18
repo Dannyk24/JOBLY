510c3ca494bb9fa1c279d52a72b007fc671fd217
Dannyk24 <teckydan@gmail.com>
2026-07-18 14:18:44 +0100

    build trusted companies homepage section component

--- src/pages/home/HomePage/HomePage.tsx
+++ src/pages/home/HomePage/HomePage.tsx
@@ -1,10 +1,12 @@
 import { HeroSection } from "../HeroSection/HeroSection";
+import { TrustedCompaniesSection } from "../TrustedCompanies/TrustedCompaniesSection";
 import "./HomePage.css";
 
 function HomePage() {
   return (
     <div className="app-page homepage">
       <HeroSection />
+      <TrustedCompaniesSection />
     </div>
   );
 }
